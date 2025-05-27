b0e7e375070cc4e8ff2f2ee182304e56a57e7c6d
Shreyam Sharma <rajroy10008@gmail.com>
2025-05-27 21:02:27 +0530

    working on edit feature

--- app.js
+++ app.js
@@ -12,14 +12,28 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.get('/', (req, res) => {
   res.render('index')
 })
+
 app.get('/read', async (req, res) => {
   let users = await userModel.find()
   res.render('read', {users})
 })
+
 app.get('/delete/:id', async (req, res) => {
   let users = await userModel.findOneAndDelete({_id: req.params.id})
   res.redirect('/read')
 })
+
+app.get('/edit/:id', async (req, res) => {
+  let user = await userModel.findOne({_id: req.params.id})
+  res.render("edit", {user})
+})
+
+app.post('/update/:id', async (req, res) => {
+  let {name, email, image} = req.body
+  let user = await userModel.findOneAndUpdate({_id: req.params.id}, {name, email, image}, {new: true})
+  res.redirect("/read")
+})
+
 app.post('/create', async (req, res) => {
     let {name, email, image} = req.body
     let createdUser = await userModel.create({
