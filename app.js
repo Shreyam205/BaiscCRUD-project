77a05a9999333486fbfca94684cf69237050b258
Shreyam Sharma <rajroy10008@gmail.com>
2025-05-27 17:05:18 +0530

    2nd update

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require('express')
 const app = express()
 const path = require('path')
-const useModel = require('./models/user')
+const userModel = require('./models/user')
+const user = require('./models/user')
 
 app.set("view engine", "ejs")
 app.use(express.json())
@@ -11,8 +12,13 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.get('/', (req, res) => {
   res.render('index')
 })
-app.get('/read', (req, res) => {
-  res.render('read')
+app.get('/read', async (req, res) => {
+  let users = await userModel.find()
+  res.render('read', {users})
+})
+app.get('/delete/:id', async (req, res) => {
+  let users = await userModel.findOneAndDelete({_id: req.params.id})
+  res.redirect('/read')
 })
 app.post('/create', async (req, res) => {
     let {name, email, image} = req.body
@@ -21,7 +27,7 @@ app.post('/create', async (req, res) => {
         email,
         image
     })
-    res.send(createdUser)
+    res.redirect("/read")
 })
 
 app.listen(3000)
