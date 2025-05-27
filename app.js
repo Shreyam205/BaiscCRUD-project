2266bf577b3eb499438009185ce3eed8fdc91df0
Shreyam Sharma <rajroy10008@gmail.com>
2025-05-28 00:14:46 +0530

    Edit feature is fully working now

--- app.js
+++ app.js
@@ -1,47 +1,47 @@
-const express = require('express')
-const app = express()
-const path = require('path')
-const userModel = require('./models/user')
-const user = require('./models/user')
+const express = require('express');
+const app = express();
+const path = require('path');
+const userModel = require('./models/user');
+const user = require('./models/user');
 
-app.set("view engine", "ejs")
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
-app.use(express.static(path.join(__dirname, 'public')))
+app.set("view engine", "ejs");
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
-  res.render('index')
+  res.render('index');
 })
 
 app.get('/read', async (req, res) => {
-  let users = await userModel.find()
-  res.render('read', {users})
+  let users = await userModel.find();
+  res.render('read', {users});
 })
 
 app.get('/delete/:id', async (req, res) => {
-  let users = await userModel.findOneAndDelete({_id: req.params.id})
-  res.redirect('/read')
+  let users = await userModel.findOneAndDelete({_id: req.params.id});
+  res.redirect('/read');
 })
 
 app.get('/edit/:id', async (req, res) => {
-  let user = await userModel.findOne({_id: req.params.id})
-  res.render("edit", {user})
+  let user = await userModel.findOne({_id: req.params.id});
+  res.render("edit", {user});
 })
 
 app.post('/update/:id', async (req, res) => {
-  let {name, email, image} = req.body
-  let user = await userModel.findOneAndUpdate({_id: req.params.id}, {name, email, image}, {new: true})
-  res.redirect("/read")
+  let {name, email, image} = req.body;
+  let user = await userModel.findOneAndUpdate({_id: req.params.id}, {name, email, image}, {new: true});
+  res.redirect("/read");
 })
 
 app.post('/create', async (req, res) => {
-    let {name, email, image} = req.body
+    let {name, email, image} = req.body;
     let createdUser = await userModel.create({
         name,
         email,
         image
-    })
-    res.redirect("/read")
+    });
+    res.redirect("/read");
 })
 
-app.listen(3000)
+app.listen(3000);
